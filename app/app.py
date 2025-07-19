eebf776c0484bbe48a2963e14d67b396e4a69e2a
svgcloudtech <svgcloudtech@gmail.com>
2025-07-19 10:43:20 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask App Deployed via GitHub Actions & Docker on EC2!"
+    return "Demo on docker and EC2!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
