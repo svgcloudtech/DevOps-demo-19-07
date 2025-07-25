1a755245dc5aa2259431082bdd2061811ee62cf8
svgcloudtech <svgcloudtech@gmail.com>
2025-07-26 00:39:59 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,31 @@
+// terraform/main.tf
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_key_pair" "deployer" {
   key_name   = "devops-key"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = var.ssh_public_key
 }
 
 resource "aws_security_group" "allow_http_ssh" {
   name        = "allow_http_ssh"
   description = "Allow HTTP and SSH"
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -35,7 +39,13 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   key_name      = aws_key_pair.deployer.key_name
   security_groups = [aws_security_group.allow_http_ssh.name]
+
   tags = {
     Name = "DevOpsDemo"
   }
+}
+
+output "ec2_public_ip" {
+  value       = aws_instance.web.public_ip
+  description = "The public IP address of the EC2 instance"
 }
